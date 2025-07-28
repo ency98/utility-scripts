85429bf6e83c143cfc9de813bbe7a34eb3682852
ency98 <59786719+ency98@users.noreply.github.com>
2025-07-28 09:25:23 +0300

    Create wan-ip.sh

---
+++ wan-ip.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#? Dig for the WAN IP
+
+WAN_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+
+GET_WAN_IT() {
+    echo -e "\n\n\033[0;32m#!#~~~~~~~~~~~~~~~~~~~~\033[0;31m Discovering WAN IP \033[0;32m~~~~~~~~~~~~~~~~~~~~#!#\n\n\033[0m"
+    echo -e "\033[0;32m                       WAN IP: \033[0;31m $WAN_IP   \033[0;32m"
+    echo -e "\n\n\033[0;32m#!# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #!#\n\n\033[0m"
+}
+
+GET_WAN_IT
